91bf9c4bf87418f9a95a2366459b6d7ddcdcf8fd
Roxeez <roxeeztsu@gmail.com>
2025-01-04 14:41:31 +0100

    add glacernon %

---
+++ TaleKit/Game/Event/Glacernon/GlacernonPercentChangedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace TaleKit.Game.Event.Glacernon;
+
+public class GlacernonPercentChangedEvent : IEvent
+{
+    public required Session Session { get; init; }
+    public required int AngelPercent { get; init; }
+    public required int DemonPercent { get; init; }
+}
---
+++ TaleKit/Network/Packet/Glacernon/Fc.cs
@@ -0,0 +1,38 @@
+﻿using TaleKit.Extension;
+using TaleKit.Game;
+using TaleKit.Game.Event.Glacernon;
+
+namespace TaleKit.Network.Packet.Glacernon;
+
+public class Fc : IPacket
+{
+    public int AngelPercent { get; set; }
+    public int DemonPercent { get; set; }
+}
+
+public class FcBuilder : PacketBuilder<Fc>
+{
+    public override string Header => "fc";
+
+    protected override Fc CreatePacket(string[] body)
+    {
+        return new Fc
+        {
+            AngelPercent = body[2].ToInt(),
+            DemonPercent = body[11].ToInt()
+        };
+    }
+}
+
+public class FcProcessor : PacketProcessor<Fc>
+{
+    protected override void Process(Session session, Fc packet)
+    {
+        session.Emit(new GlacernonPercentChangedEvent
+        {
+            Session = session,
+            AngelPercent = packet.AngelPercent,
+            DemonPercent = packet.DemonPercent
+        });
+    }
+}
