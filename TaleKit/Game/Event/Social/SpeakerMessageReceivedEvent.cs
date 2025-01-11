6750a277c9fb78b639984bb49756c9ea72cc85ca
Roxeez <roxeeztsu@gmail.com>
2025-01-11 03:13:31 +0100

    add speaker event

---
+++ TaleKit/Game/Event/Social/SpeakerMessageReceivedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace TaleKit.Game.Event.Social;
+
+public class SpeakerMessageReceivedEvent : IEvent
+{
+    public required Session Session { get; init; }
+    
+    public required string Message { get; init; }
+    public required string Sender { get; init; }
+}
---
+++ TaleKit/Network/Packet/Interaction/Sayt.cs
@@ -0,0 +1,45 @@
+﻿using TaleKit.Extension;
+using TaleKit.Game;
+using TaleKit.Game.Event.Social;
+
+namespace TaleKit.Network.Packet.Interaction;
+
+public class Sayt : IPacket
+{
+    public string Message { get; init; }
+    public string Sender { get; init; }
+    public int Kind { get; set; }
+}
+
+public class SaytBuilder : PacketBuilder<Sayt>
+{
+    public override string Header { get; } = "sayt";
+    
+    protected override Sayt CreatePacket(string[] body)
+    {
+        var message = string.Join(" ", body.Skip(4).ToArray());
+
+        return new Sayt
+        {
+            Kind = body[2].ToInt(),
+            Sender = body[3],
+            Message = message
+        };
+    }
+}
+
+public class SaytProcessor : PacketProcessor<Sayt>
+{
+    protected override void Process(Session session, Sayt packet)
+    {
+        if (packet.Kind == 13)
+        {
+            session.Emit(new SpeakerMessageReceivedEvent
+            {
+                Session = session,
+                Sender = packet.Sender,
+                Message = packet.Message
+            });
+        }
+    }
+}
