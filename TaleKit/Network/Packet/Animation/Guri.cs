b673108f610228ac63990b605c1424766015bab5
Roxeez <roxeeztsu@gmail.com>
2025-02-18 11:02:29 +0100

    fix guri

--- TaleKit/Network/Packet/Animation/Guri.cs
+++ TaleKit/Network/Packet/Animation/Guri.cs
@@ -13,15 +13,15 @@ public class Guri : IPacket
 
 public class GuriBuilder : PacketBuilder<Guri>
 {
-    public override string Header { get; } = "guri";
-    
+    public override string Header => "guri";
+
     protected override Guri CreatePacket(string[] body)
     {
         return new Guri
         {
-            Id = body[0].ToInt(),
+            Id = body.Length >= 1 ? body[0].ToInt() : 0,
             EntityType = body.Length >= 2 ? body[1].ToEnum<EntityType>() : 0,
-            EntityId =  body.Length >= 3 ?body[2].ToInt() : 0,
+            EntityId =  body.Length >= 3 ? body[2].ToInt() : 0,
             AnimationId = body.Length >= 4 ? body[3].ToInt() : 0
         };
     }
