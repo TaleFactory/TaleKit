c74ecbf04979765a0fd4df46c06f48af1e7765b0
Roxeez <roxeeztsu@gmail.com>
2025-07-18 13:14:18 +0200

    feat: add srvlist

---
+++ TaleKit/Network/Packet/Login/SrvList.cs
@@ -0,0 +1,15 @@
+﻿namespace TaleKit.Network.Packet.Login;
+
+public class SrvList : IPacket
+{
+    
+}
+
+public class SrvListBuilder : PacketBuilder<SrvList>
+{
+    public override string Header { get; } = "svrlist";
+    protected override SrvList CreatePacket(string[] body)
+    {
+        return new SrvList();
+    }
+}
