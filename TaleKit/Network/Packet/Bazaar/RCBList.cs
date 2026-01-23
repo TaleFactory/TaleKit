f3a9bf042ebff34f9eae8a4ecbddc4d744f8fbd7
Roxeez <roxeeztsu@gmail.com>
2026-01-23 20:17:41 +0100

    Adds RCBList packet for bazaar listings
    Adds the RCBList packet structure and builder to handle bazaar listing data.
    
    This allows the server to transmit a list of bazaar entries
    to the client. The packet includes information such as owner,
    item ID, amount, and price for each listing.

---
+++ TaleKit/Network/Packet/Bazaar/RCBList.cs
@@ -0,0 +1,50 @@
+﻿using TaleKit.Extension;
+using TaleKit.Game;
+
+namespace TaleKit.Network.Packet.Bazaar;
+
+public class RCBList : IPacket
+{
+    public List<RCBListEntry> Entries { get; init; } = new();
+}
+
+public class RCBListEntry
+{
+    public required string Owner { get; init; }
+    public required int ItemId { get; init; }
+    public required int Amount { get; init; }
+    public required int Price { get; init; }
+}
+
+public class RCBListBuilder : PacketBuilder<RCBList>
+{
+    public override string Header => "rc_blist";
+    
+    protected override RCBList CreatePacket(string[] body)
+    {
+        var entries = new List<RCBListEntry>();
+        var values = body.Skip(1).ToArray();
+        foreach (var value in values)
+        {
+            var split = value.Split('|');
+
+            var name = split[2];
+            var itemId = split[3].ToInt();
+            var amount = split[4].ToInt();
+            var price = split[6].ToInt();  
+            
+            entries.Add(new RCBListEntry
+            {
+                Owner = name,
+                ItemId = itemId,
+                Amount = amount,
+                Price = price
+            });
+        }
+
+        return new RCBList
+        {
+            Entries = entries
+        };
+    }
+}
